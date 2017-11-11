46724a6c1316c15693ed88daab6e9b4c8fd7b5cd
frankrb27 <32852903+frankrb27@users.noreply.github.com>
2017-11-10 22:04:59 -0500

    Add files via upload

---
+++ TallerBancoABC/PagoConvenio/src/main/java/co/edu/javeriana/modval/PagoConvenioApplication.java
@@ -0,0 +1,12 @@
+package co.edu.javeriana.modval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoConvenioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoConvenioApplication.class, args);
+	}
+}
---
+++ TallerBancoABC/PagoConvenio/src/main/java/co/edu/javeriana/modval/entities/Factura.java
@@ -0,0 +1,62 @@
+package co.edu.javeriana.modval.entities;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Factura {
+
+    private String idFactura;
+    private double valorFactura;
+	
+    /**
+     * Constructor
+     */
+    public Factura() {}
+    
+    /**
+     * Constructor
+     * @param valorFactura
+     */
+    public Factura(double valorFactura) {
+        this.valorFactura = valorFactura;
+    }
+
+    /**
+     * Constructor
+     * @param idFactura
+     * @param valorFactura
+     */
+    public Factura(String idFactura, double valorFactura) {
+        this.idFactura = idFactura;
+        this.valorFactura = valorFactura;
+    }    
+    
+    /**
+	 * @return the idFactura
+	 */
+	public String getIdFactura() {
+		return idFactura;
+	}
+	
+	/**
+	 * @param idFactura the idFactura to set
+	 */
+	@XmlElement
+	public void setIdFactura(String idFactura) {
+		this.idFactura = idFactura;
+	}
+	/**
+	 * @return the valorFactura
+	 */
+	public double getValorFactura() {
+		return valorFactura;
+	}
+	/**
+	 * @param valorFactura the valorFactura to set
+	 */
+	@XmlElement
+	public void setValorFactura(double valorFactura) {
+		this.valorFactura = valorFactura;
+	}
+}
