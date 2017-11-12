35bfa48f99db2e4f75c8b2f90ebaf3464f49652c
frankrb27 <32852903+frankrb27@users.noreply.github.com>
2017-11-12 13:54:00 -0500

    Add files via upload

--- TallerBancoABC/PagoConvenio/src/main/java/co/edu/javeriana/modval/control/PagoConvenioService.java
+++ TallerBancoABC/PagoConvenio/src/main/java/co/edu/javeriana/modval/control/PagoConvenioService.java
@@ -1,7 +1,6 @@
 package co.edu.javeriana.modval.control;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.Arrays;
@@ -27,7 +26,7 @@ import co.edu.javeriana.modval.entities.Respuesta;
 
 @Service
 public class PagoConvenioService {
-	
+	private static String TEMPLATE_JSON = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"><xsl:output method=\"text\" encoding=\"utf-8\"/><xsl:template match=\"/*[node()]\"><xsl:text>{</xsl:text><xsl:apply-templates select=\".\" mode=\"detect\" /><xsl:text>}</xsl:text></xsl:template><xsl:template match=\"*\" mode=\"detect\"><xsl:choose><xsl:when test=\"name(preceding-sibling::*[1]) = name(current()) and name(following-sibling::*[1]) != name(current())\"><xsl:apply-templates select=\".\" mode=\"obj-content\" /><xsl:text>]</xsl:text><xsl:if test=\"count(following-sibling::*[name() != name(current())]) &gt; 0\">, </xsl:if></xsl:when><xsl:when test=\"name(preceding-sibling::*[1]) = name(current())\"><xsl:apply-templates select=\".\" mode=\"obj-content\" /><xsl:if test=\"name(following-sibling::*) = name(current())\">, </xsl:if></xsl:when><xsl:when test=\"following-sibling::*[1][name() = name(current())]\"><xsl:text>\"</xsl:text><xsl:value-of select=\"name()\"/><xsl:text>\" : [</xsl:text><xsl:apply-templates select=\".\" mode=\"obj-content\" /><xsl:text>, </xsl:text></xsl:when><xsl:when test=\"count(./child::*) > 0 or count(@*) > 0\"><xsl:text>\"</xsl:text><xsl:value-of select=\"name()\"/>\" : <xsl:apply-templates select=\".\" mode=\"obj-content\" /><xsl:if test=\"count(following-sibling::*) &gt; 0\">, </xsl:if></xsl:when><xsl:when test=\"count(./child::*) = 0\"><xsl:text>\"</xsl:text><xsl:value-of select=\"name()\"/>\" : \"<xsl:apply-templates select=\".\"/><xsl:text>\"</xsl:text><xsl:if test=\"count(following-sibling::*) &gt; 0\">, </xsl:if></xsl:when></xsl:choose></xsl:template><xsl:template match=\"*\" mode=\"obj-content\"><xsl:text>{</xsl:text><xsl:apply-templates select=\"@*\" mode=\"attr\" /><xsl:if test=\"count(@*) &gt; 0 and (count(child::*) &gt; 0 or text())\">, </xsl:if><xsl:apply-templates select=\"./*\" mode=\"detect\" /><xsl:if test=\"count(child::*) = 0 and text() and not(@*)\"><xsl:text>\"</xsl:text><xsl:value-of select=\"name()\"/>\" : \"<xsl:value-of select=\"text()\"/><xsl:text>\"</xsl:text></xsl:if><xsl:if test=\"count(child::*) = 0 and text() and @*\"><xsl:text>\"text\" : \"</xsl:text><xsl:value-of select=\"text()\"/><xsl:text>\"</xsl:text></xsl:if><xsl:text>}</xsl:text><xsl:if test=\"position() &lt; last()\">, </xsl:if></xsl:template><xsl:template match=\"@*\" mode=\"attr\"><xsl:text>\"</xsl:text><xsl:value-of select=\"name()\"/>\" : \"<xsl:value-of select=\".\"/><xsl:text>\"</xsl:text><xsl:if test=\"position() &lt; last()\">,</xsl:if></xsl:template><xsl:template match=\"node/@TEXT | text()\" name=\"removeBreaks\"><xsl:param name=\"pText\" select=\"normalize-space(.)\"/><xsl:choose><xsl:when test=\"not(contains($pText, '&#xA;'))\"><xsl:copy-of select=\"$pText\"/></xsl:when><xsl:otherwise><xsl:value-of select=\"concat(substring-before($pText, '&#xD;&#xA;'), ' ')\"/><xsl:call-template name=\"removeBreaks\"><xsl:with-param name=\"pText\" select=\"substring-after($pText, '&#xD;&#xA;')\"/></xsl:call-template></xsl:otherwise></xsl:choose></xsl:template></xsl:stylesheet>";
 	public Convenio getConvenio(String idFactura){
 		if(idFactura.equals("10000")){
 		return new Convenio(
@@ -131,8 +130,7 @@ public class PagoConvenioService {
 	 */
 	private String getJson(String xml) throws Exception{
 	    //Cargar template XML to JSON
-		File file = new File("XMLJSON");
-		StreamSource styleJson = new StreamSource(new File(file.getAbsolutePath().replace("XMLJSON","")+"json.xsl"));
+		StreamSource styleJson = new StreamSource((InputStream)(new ByteArrayInputStream(TEMPLATE_JSON.getBytes("UTF-8"))));
 		TransformerFactory factory = TransformerFactory.newInstance();
 	    Transformer transformer = factory.newTransformer(styleJson);
 	    StringWriter writer = new StringWriter();
